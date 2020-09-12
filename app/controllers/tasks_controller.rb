4be232e4081b48d4fbe9543a3f210aaa18fb6f95
katsuya Ushida <katsuya.u0409@gmail.com>
2020-09-12 15:39:34 +0900

    add new , flash

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,30 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task), notice: 'Task is create'
+    else
+      flash.now[:error] = 'save failure'
+      render :new
+    end
+  end
+
   def edit
     @task = Task.find(params[:id])
   end
 
   def destroy
     @task = Task.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline)
   end
 end
