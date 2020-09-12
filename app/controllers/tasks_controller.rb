30c8a8869baa7dd27d4bb24f8e4d38d8c88d784f
katsuya Ushida <katsuya.u0409@gmail.com>
2020-09-12 16:01:22 +0900

    add edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,6 +25,16 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Task is update'
+    else
+      flash.now[:error] = 'update failure'
+      render :edit
+    end
+  end
+
   def destroy
     @task = Task.find(params[:id])
   end
